1c9014507af77f6ace6eaeabbdf927e21d101cbc
Iamshasi <shashikumar9515@gmail.com>
2022-05-10 13:49:43 +0530

    Initial Commit

---
+++ src/test/java/Project/Test1.java
@@ -0,0 +1,21 @@
+package Project;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void test1() {
+		
+		Assert.assertEquals(true, true);
+	}
+ 
+		
+		@Test
+		public void tes21() {
+			
+			Assert.assertEquals(false, false);
+		}
+
+}
